6b493ca461245d6cc8607ab6cb3d15656159d9ac
Tereshin A <teran4work@gmail.com>
2015-10-30 12:23:49 +0300

    V2

--- data.js
+++ data.js
@@ -1,26 +1,21 @@
 var bee = bee || {};
 bee.data = bee.data || [];
 bee.data.push({
-  trigger: ".form-inline .submit .CalculateButton span.disabled", // Required. Элемент, на который устанавливается событие
-  content: null, // Если null, то берется значения из активного trigger, нужно также передавать скрипты
-  data: {
-    eventname: null,// Если null, берется дефолтное значение
-    category: "commonClick", // Required. Категория события, также является именем контроллера
-    action: null, // Если null, определяетя алгоритмом контроллера
-    label: null, // Если null, берется дефолтное значение
-    context: null // Если null, определяетя алгоритмом контроллера
-  },
-  cut: {
-    text: { // Переменная, для которой необходимо обрезание текста
-      start: null, // Если null, то 0
-      end: 11 // Если null, то до конца строки
+    trigger: ".form-inline .submit .CalculateButton span.disabled", // Required. Элемент, на который устанавливается событие
+    content: null, // Если null, то берется значения из активного trigger, нужно также передавать скрипты
+    data: {
+        eventname: null, // Если null, берется дефолтное значение
+        category: "commonClick", // Required. Категория события, также является именем контроллера
+        action: null, // Если null, определяетя алгоритмом контроллера
+        label: null, // Если null, берется дефолтное значение
+        context: null // Если null, определяетя алгоритмом контроллера
     },
-    action: {
-      end: 16  
-      }
+    cut: {
+        start: null, // Если null, то 0
+        end: 11 // Если null, то до конца строки
     },
-  replace: {
-    regexp: /hon/i,
-    to: 'blablabla'
-  },
+    replace: {
+        regexp: /hon/i,
+        to: 'blablabla'
+    },
 });
---
+++ v2.js
@@ -0,0 +1,197 @@
+window.bee = window.bee || {};
+(function(bee) {
+
+    var debug = false;
+
+    function getContent(trigger, selector) {
+        if (!(this instanceof getContent)) {
+            return new getContent(trigger, selector);
+        }
+        if (selector != undefined) {
+            this.planeText = $(trigger).find(selector).text();
+        } else {
+            this.planeText = $(trigger).text();
+        }
+        if (this.planeText == undefined && this.planeText == "" && $(trigger).find("input[type=submit]")) {
+            this.planeText = $(trigger).find("input[type=submit]").attr("value");
+        }
+        if (this.planeText == undefined && this.planeText == "" && $(trigger).find("input[type=button]")) {
+            this.planeText = $(trigger).find("input[type=submit]").attr("value");
+        }
+        if (this.planeText == undefined && this.planeText == "" && $(trigger).find("img")) {
+            this.planeText = $(trigger).find("img").attr("alt");
+        }
+        return this;
+    };
+
+    getContent.prototype.getText = function() {
+        return this.planeText;
+    };
+
+    getContent.prototype.replace = function(params) {
+        if (!params) return this;
+        if (params.to == undefined) params.to = "";
+        if (params.regexp instanceof RegExp) {
+            this.planeText = this.planeText.replace(params.regexp, params.to)
+        } else {
+            throw Error('Invalid RegExp: ' + params.regexp);
+        }
+        return this;
+    };
+
+    getContent.prototype.cut = function(params) {
+        if (this.planeText != undefined && params && params.length !== undefined) {
+            var text = this.planeText;
+            this.planeText = text.substring(params.start, params.end);
+        }
+        return this;
+    };
+
+    getContent.prototype.trim = function() {
+        if (this.planeText != undefined) {
+            var text = this.planeText;
+            text = text.replace(/\r\n|\n|\r|\s/gm, " ");
+            text = text.replace(/^(\s|\u00A0)+|(\s|\u00A0)+$/g, " ");
+            text = text.replace(/\s+(?=\s)/g, " ");
+            text = text.trim();
+            this.planeText = text;
+        }
+        return this;
+    };
+
+    var getNth = function(element) {
+        var sibbling = element;
+        var n = 1,
+            i = 1;
+        while ((sibbling = sibbling.previousSibling) && i++) {
+            if (sibbling.nodeType === Node.ELEMENT_NODE) n++
+        }
+        var nth = n;
+        return nth;
+    }
+
+    function pushData(cb) {
+        var data = cb();
+        if (Object.prototype.toString.call(window.dataLayer) !== "[object Array]") {
+            throw Error("Wrong instanceof of dataLayer ", window.dataLayer);
+        }
+        if (data) {
+            data.event = data.event != undefined ? data.event : "GA_event";
+            //data.eventLabel = data.eventLabel != undefined ? data.eventLabel : {
+            //{
+            //URL Host - Path
+            //}
+            //}
+            window.dataLayer.push(data);
+            if (debug) console.log("pushData ", data);
+        }
+    };
+
+    function collectInfo(target, collection, trigger) {
+        if (debug) console.log('Start collecting information for object: ', collection);
+
+        var adapter, category,
+            place = collection.selector.replace(/\.|\#|\>/gm, ""),
+            controller = (collection.eventdata && collection.eventdata.category) ? collection.eventdata.category : false,
+            content = new getContent(trigger, collection.content)
+            .replace(collection.replace)
+            .trim()
+            .cut(collection.cut)
+            .getText();
+        if (controller && controller === "linkClick") {
+            adapter = function() {
+                var category = collection.eventdata.category,
+                    action = (collection.eventdata && collection.eventdata.action) ? collection.eventdata.action : content + ' / ' + place,
+                    context = $(trigger).attr("href");
+                return {
+                    "eventCategory": category,
+                    "eventAction": action,
+                    "eventContext": context
+                }
+            }
+        } else if (controller && controller === "openReclame") {
+            adapter = function() {
+                var context = getNth(trigger),
+                    category = collection.eventdata.category,
+                    action = collection.eventdata.action + " / " + content;
+                return {
+                    "eventCategory": category,
+                    "eventAction": action,
+                    "eventContext": context
+                };
+            }
+        } else if (controller && controller === "hiddenInteraction") {
+            adapter = function() {
+                var category = $(trigger).hasClass('opened') ? 'closeHidden' : 'openHidden',
+                    action = collection.eventdata.action ? collection.eventdata.action : content + ' / ' + place;
+                return {
+                    "eventCategory": category,
+                    "eventAction": action
+                };
+            }
+        } else if (controller && controller === "checkInteraction") {
+            adapter = function() {
+                var category,
+                    action = collection.eventdata.action ? collection.eventdata.action : content + ' / ' + place,
+                    checkbox = $(trigger).find(".checkbox") || $(trigger).find(".checkbox-slide") || trigger;
+                if (!checkbox.hasClass("inactive")) {
+                    if (checkbox.hasClass("checkbox-slide")) {
+                        category = checkbox.hasClass("checked") ? "checkOn" : "checkOff";
+                    } else {
+                        category = checkbox.hasClass("checked") ? "checkOff" : "checkOn";
+                    }
+                }
+                return {
+                    "eventCategory": category,
+                    "eventAction": action
+                };
+            }
+        } else {
+            adapter = function() {
+                var category = (collection.eventdata && collection.eventdata.category) ? collection.eventdata.category : "commonClick",
+                    action = (collection.eventdata && collection.eventdata.action) ? collection.eventdata.action : content + ' / ' + place;
+                return {
+                    "eventCategory": category,
+                    "eventAction": action
+                }
+            }
+        }
+        pushData(adapter);
+    }
+
+    function checkTargetComplies(data, target) {
+        if (!data || !target) {
+            throw Error('Invalid arguments: ' + data + ' ' + target);
+        }
+        for (var i = 0; i < data.length; i++) {
+            for (
+                var elem = target; elem && elem != this && elem.parentNode && elem.parentNode.tagName != "BODY"; elem = elem.parentNode
+            ) {
+                if (debug) console.log(i, data[i].selector, elem.matches(data[i].selector));
+                if (typeof elem.matches == 'function' && elem.matches(data[i].selector)) {
+                    if (debug) console.log("Complies have been found");
+                    return {
+                        collection: data[i],
+                        trigger: elem
+                    }
+                }
+            }
+        }
+        return false;
+    };
+
+    var ClickTracking = new function() {
+        $(document).on("mousedown touchstart", function(e) {
+            if (debug) console.log("event:", e);
+            var triggerdata = checkTargetComplies(bee.data, e.target);
+            triggerdata && collectInfo(e.target, triggerdata.collection, triggerdata.trigger);
+        });
+    };
+
+    var AjaxTracking = new function() {
+
+    };
+    bee.getContent = getContent;
+    bee.pushData = pushData;
+
+}(window.bee));
